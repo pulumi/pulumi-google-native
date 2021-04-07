51d0d25fbf51adcaa28f0aeb9fa743be4fd11272
Mikhail Shilkov <github@mikhail.io>
2021-04-07 17:13:14 +0200

    Add a testable Cloud Run example

--- examples/examples_nodejs_test.go
+++ examples/examples_nodejs_test.go
@@ -1,4 +1,4 @@
-// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2021, Pulumi Corporation.  All rights reserved.
 // +build nodejs all
 
 package examples
@@ -8,6 +8,15 @@ import (
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
+
+func TestCloudRunTs(t *testing.T) {
+	test := getJSBaseOptions(t).
+		With(integration.ProgramTestOptions{
+			Dir: filepath.Join(getCwd(t), "cloudrun-ts"),
+		})
+
+	integration.ProgramTest(t, &test)
+}
 
 func getJSBaseOptions(t *testing.T) integration.ProgramTestOptions {
 	base := getBaseOptions(t)
