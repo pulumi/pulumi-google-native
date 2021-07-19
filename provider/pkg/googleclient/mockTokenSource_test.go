918cf7a4d8bde089228c56eba9088a04b3ed6fe7
Vivek Lakshmanan <vivek@pulumi.com>
2021-07-18 20:42:29 -0700

    Add mock for token source

---
+++ provider/pkg/googleclient/mockTokenSource_test.go
@@ -0,0 +1,34 @@
+package googleclient
+
+import (
+	"github.com/stretchr/testify/mock"
+	"golang.org/x/oauth2"
+)
+
+// MockTokenSource is a mock type for the oauth2.TokenSource type
+type MockTokenSource struct {
+	mock.Mock
+}
+
+// Token provides a mock function with given fields:
+func (_m *MockTokenSource) Token() (*oauth2.Token, error) {
+	ret := _m.Called()
+
+	var r0 *oauth2.Token
+	if rf, ok := ret.Get(0).(func() *oauth2.Token); ok {
+		r0 = rf()
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(*oauth2.Token)
+		}
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func() error); ok {
+		r1 = rf()
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
