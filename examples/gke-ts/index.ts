68c1fdefe68fdba9e2a9a253a2cdc28e116f9d63
Levi Blackstone <levi@pulumi.com>
2022-02-02 17:09:17 -0700

    Use supported GKE version

--- examples/gke-ts/index.ts
+++ examples/gke-ts/index.ts
@@ -1,9 +1,23 @@
-// Copyright 2016-2021, Pulumi Corporation.
+/*
+ * Copyright 2016-2022, Pulumi Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 import * as google from "@pulumi/google-native";
 
 // TODO: Determine this dynamically once https://github.com/pulumi/pulumi-google-native/issues/166 is done.
-const engineVersion = "1.20.10-gke.1600";
+const engineVersion = "1.21.6-gke.1500";
 
 const nodeConfig: google.types.input.container.v1.NodeConfigArgs = {
     machineType: "n1-standard-2",
