0d7820eedbc066361368b05a0f2673f89ab2393c
Levi Blackstone <levi@pulumi.com>
2022-04-20 14:49:05 -0600

    Add test

--- examples/examples_test.go
+++ examples/examples_test.go
@@ -1,10 +1,21 @@
-// Copyright 2021, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2022, Pulumi Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 package examples
 
 import (
 	"fmt"
-	"github.com/stretchr/testify/assert"
 	"io/ioutil"
 	"net/http"
 	"os"
@@ -13,6 +24,7 @@ import (
 	"time"
 
 	"github.com/pulumi/pulumi/pkg/v3/testing/integration"
+	"github.com/stretchr/testify/assert"
 )
 
 func getProject(t *testing.T) string {
@@ -26,9 +38,6 @@ func getProject(t *testing.T) string {
 
 func getZone(t *testing.T) string {
 	zone := os.Getenv("GOOGLE_ZONE")
-	if zone == "" {
-		t.Skipf("Skipping test due to missing GOOGLE_ZONE environment variable")
-	}
 
 	return zone
 }
