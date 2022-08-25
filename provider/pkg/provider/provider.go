pulumi/pulumi-google-native

commit 5634547b592d00387d9c6fed93195af9df3fd4a0

Custom mutations for v1/v1beta1 cluster and nodepool (#632)
* Adding custom mutations for v1/v1beta1 cluster and nodepool

* Schema changes

* Cluster resting status checks

* Use deep diff keys to look up mutations

* Add comments

* Changelog

* Update test

* More test fixes

provider/pkg/provider/provider.go
@@ -466,15 +466,19 @@ func (p *googleCloudProvider) Diff(_ context.Context, req *rpc.DiffRequest) (*rp
 	if customMutation, hasCustomMutation := customMutations[resourceKey]; hasCustomMutation {
 		mutablePropertyPaths = customMutation.mutablePropertyPaths()
 	}
+	logging.V(9).Infof("[%s] DetailedDiff: %+v", label, detailedDiff)
+	changedKeys, err := diffWithFlattenedKeys(oldInputs, newInputs, detailedDiff)
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to flatten keys for detailed diff")
+	}
 	// Based on the detailed diff above, calculate the list of changes and replacements.
 	var changes []string
 	replaces := codegen.NewStringSet()
-	for k, v := range detailedDiff {
+	for k, kind := range changedKeys {
 		parts := strings.Split(k, ".")
 		changes = append(changes, parts[0])
-		v.InputDiff = true
 
-		switch v.Kind {
+		switch kind {
 		case rpc.PropertyDiff_ADD_REPLACE, rpc.PropertyDiff_DELETE_REPLACE, rpc.PropertyDiff_UPDATE_REPLACE:
 			replaces.Add(k)
 			continue

examples/examples_nodejs_test.go
@@ -21,11 +21,15 @@ import (
 	"path/filepath"
 	"testing"
 
+	"github.com/pulumi/pulumi/pkg/v3/codegen"
 	"github.com/pulumi/pulumi/pkg/v3/testing/integration"
+	"github.com/pulumi/pulumi/sdk/v3/go/common/apitype"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestGKE(t *testing.T) {
+	clusterDiffs := codegen.NewStringSet("resourceLabels")
+	nodepoolDiffs := codegen.NewStringSet("config")
 	test := getJSBaseOptions(t).
 		With(integration.ProgramTestOptions{
 			Dir:           filepath.Join(getCwd(t), "gke-ts", "step1"),
@@ -36,12 +40,28 @@ func TestGKE(t *testing.T) {
 					Dir:      filepath.Join(getCwd(t), "gke-ts", "step2"),
 					Additive: true,
 					ExtraRuntimeValidation: func(t *testing.T, stack integration.RuntimeValidationStackInfo) {
+						foundClusterUpdate, foundNodepoolUpdate := false, false
+						for _, event := range stack.Events {
+							if res := event.ResOutputsEvent; res != nil && res.Metadata.Op == apitype.OpUpdate {
+								if res.Metadata.Type == "google-native:container/v1:Cluster" {
+									for _, field := range res.Metadata.Diffs {
+										if clusterDiffs.Has(field) {
+											foundClusterUpdate = true
+										}
+									}
+								}
+								if res.Metadata.Type == "google-native:container/v1:NodePool" {
+									for _, field := range res.Metadata.Diffs {
+										if nodepoolDiffs.Has(field) {
+											foundNodepoolUpdate = true
+										}
+									}
+								}
+							}
+						}
+						assert.True(t, foundClusterUpdate, "should have seen a cluster update")
+						assert.True(t, foundNodepoolUpdate, "should have seen a nodepool update")
 						t.Logf("outputs: %+v", stack.Outputs)
-						nodepoolTag, ok := stack.Outputs["nodepoolTag"]
-						assert.True(t, ok)
-						assert.Equal(t, "nodepool", nodepoolTag)
-						taintsKey, ok := stack.Outputs["taintsKey"]
-						assert.Equal(t, "important", taintsKey)
 					},
 				},
 			},

examples/gke-ts/step2/index.ts
@@ -40,6 +40,8 @@ const cluster = new google.container.v1.Cluster("cluster", {
         },
         name: "initial",
     }],
+    // Change
+    resourceLabels: {"test": "true"},
 });
 
 const nodepool = new google.container.v1.NodePool("nodepool", {

provider/pkg/gen/overrides.go
@@ -265,6 +265,34 @@ var metadataOverrides = map[string]resources.CloudAPIResource{
 			CloudAPIOperation: resources.CloudAPIOperation{
 				Polling: &resources.Polling{Strategy: resources.ClusterAwaitRestingStatePoll},
 			},
+			RecordDefaults: map[string]resources.CloudAPIProperty{
+				"loggingService":                        {},
+				"monitoringService":                     {},
+				"initialClusterVersion":                 {},
+				"locations":                             {},
+				"networkConfig.privateIpv6GoogleAccess": {},
+				"nodeConfig.imageType":                  {},
+			},
+		},
+		Update: resources.UpdateAPIOperation{
+			CloudAPIOperation: resources.CloudAPIOperation{
+				Polling: &resources.Polling{Strategy: resources.ClusterAwaitRestingStatePoll},
+			},
+		},
+	},
+	"google-native:container/v1beta1:Cluster": {
+		Create: resources.CreateAPIOperation{
+			CloudAPIOperation: resources.CloudAPIOperation{
+				Polling: &resources.Polling{Strategy: resources.ClusterAwaitRestingStatePoll},
+			},
+			RecordDefaults: map[string]resources.CloudAPIProperty{
+				"loggingService":                        {},
+				"monitoringService":                     {},
+				"initialClusterVersion":                 {},
+				"locations":                             {},
+				"networkConfig.privateIpv6GoogleAccess": {},
+				"nodeConfig.imageType":                  {},
+			},
 		},
 		Update: resources.UpdateAPIOperation{
 			CloudAPIOperation: resources.CloudAPIOperation{
@@ -280,6 +308,24 @@ var metadataOverrides = map[string]resources.CloudAPIResource{
 			RecordDefaults: map[string]resources.CloudAPIProperty{
 				"version":          {},
 				"config.imageType": {},
+				"locations":        {},
+			},
+		},
+		Update: resources.UpdateAPIOperation{
+			CloudAPIOperation: resources.CloudAPIOperation{
+				Polling: &resources.Polling{Strategy: resources.NodepoolAwaitRestingStatePoll},
+			},
+		},
+	},
+	"google-native:container/v1beta1:NodePool": {
+		Create: resources.CreateAPIOperation{
+			CloudAPIOperation: resources.CloudAPIOperation{
+				Polling: &resources.Polling{Strategy: resources.NodepoolAwaitRestingStatePoll},
+			},
+			RecordDefaults: map[string]resources.CloudAPIProperty{
+				"version":          {},
+				"config.imageType": {},
+				"locations":        {},
 			},
 		},
 		Update: resources.UpdateAPIOperation{

provider/pkg/provider/container.go
@@ -17,7 +17,10 @@ package provider
 import (
 	"context"
 	"fmt"
+	"strings"
 	"time"
+
+	"github.com/imdario/mergo"
 
 	"github.com/pulumi/pulumi/pkg/v3/codegen"
 
@@ -30,8 +33,335 @@ import (
 	"github.com/pulumi/pulumi/sdk/v3/go/common/util/logging"
 )
 
+// Container cluster support
+type clusterMutations struct {
+	updateHandlers map[string]containerCustomUpdateHandlerFunc
+}
+
+func (c clusterMutations) mutablePropertyPaths() []string {
+	return codegen.SortedKeys(c.updateHandlers)
+}
+
+func (c clusterMutations) update(
+	providerInstance *googleCloudProvider,
+	urn resource.URN,
+	label string,
+	res *resources.CloudAPIResource,
+	inputs resource.PropertyMap,
+	oldState resource.PropertyMap,
+) (appliedInputs resource.PropertyMap, resp map[string]interface{}, err error) {
+	// Extract old inputs from the `__inputs` field of the old state.
+	oldInputs := parseCheckpointObject(oldState)
+	// Delete the auto-injected __autonamed tag in old and new inputs to avoid spurious diffs.
+	if v, ok := oldInputs[autonamed]; ok && v.IsBool() {
+		delete(oldInputs, autonamed)
+	}
+
+	if v, ok := inputs[autonamed]; ok && v.IsBool() {
+		delete(inputs, autonamed)
+	}
+
+	diff := oldInputs.Diff(inputs)
+	batchedInputs := resource.NewObjectProperty(oldInputs)
+
+	if diff == nil {
+		logging.V(9).Infof("[%s] no diff found in update!", label)
+	} else {
+		// Calculate the detailed diff object containing information about replacements.
+		detailedDiff := calculateDetailedDiff(res, providerInstance.resourceMap.Types, diff)
+		logging.V(9).Infof("[%s] detailedDiff: %+v", label, detailedDiff)
+		logging.V(9).Infof("[%s] updateCluster oldState: %+v", label, oldState)
+
+		for _, key := range clusterUpdateOrder {
+			updateHandler, ok := c.updateHandlers[key]
+			if !ok {
+				logging.V(9).Infof("[%s] No handler found for field: %q", label, key)
+				// TODO This should be an error once all the targetted mutations are in place!
+				continue
+			}
+			logging.V(9).Infof("[%s] Invoking update for field: %q", label, key)
+			for k := range detailedDiff {
+				logging.V(9).Infof("[%s] processing diff on field: %q", label, k)
+				var diffPropPath, propPath resource.PropertyPath
+				diffPropPath, err = resource.ParsePropertyPath(k)
+				if err != nil {
+					err = fmt.Errorf("failed to parse property path: %q: %w", k, err)
+					break
+				}
+				propPath, err = resource.ParsePropertyPath(key)
+				if err != nil {
+					err = fmt.Errorf("failed to parse property path: %q: %w", k, err)
+					break
+				}
+				if propPath.Contains(diffPropPath) {
+					_ = providerInstance.host.LogStatus(context.Background(), diag.Info, urn,
+						fmt.Sprintf("Performing update for field: %q", key))
+					logging.V(9).Infof("[%s] processing diff for %q with handler for %q",
+						label, diffPropPath.String(), propPath.String())
+					logging.V(9).Infof("[%s] waiting for cluster to reach resting state", label)
+					_, err = waitForClusterRestingState(providerInstance, urn, res, inputs, oldState)
+					if err != nil {
+						break
+					}
+					newVal, _ := diffPropPath.Get(resource.NewObjectProperty(inputs))
+					logging.V(9).Infof("[%s] calling update handler", label)
+					err = updateHandler(providerInstance, urn, label, res, inputs, oldState)
+					if err != nil {
+						logging.V(9).Infof("[%s] failure updating: %+v", label, err)
+						break
+					}
+					batchedInputs, ok = diffPropPath.Add(batchedInputs, newVal)
+					if !ok {
+						err = fmt.Errorf("failed to record update at field: %q", k)
+						break
+					}
+				}
+			}
+			if err != nil {
+				break
+			}
+		}
+	}
+	appliedInputs = batchedInputs.ObjectValue()
+	uri, readErr := res.Read.Endpoint.URI(inputs.Mappable(), oldState.Mappable())
+	if readErr != nil {
+		return nil, nil, readErr
+	}
+	resp, err2 := readContainerResourceStatus(providerInstance, uri)
+	if err2 != nil {
+		logging.V(9).Infof("[%s] Failed to read cluster status: %v", label, err2)
+		return nil, nil, fmt.Errorf("failed to retrieve cluster status: %v. Previous error: %w", err2, err)
+	}
+
+	return appliedInputs, resp, err
+}
+
+func updateClusterMapping(
+	diffPropPath resource.PropertyPath,
+	apiFieldName string,
+	defaultIfMissing propValueIfMissingFunc,
+	operationSuffix string,
+	httpMethod string,
+	additionalMetadata map[string]resources.CloudAPIProperty,
+) containerCustomUpdateHandlerFunc {
+	return func(p *googleCloudProvider,
+		urn resource.URN,
+		label string,
+		res *resources.CloudAPIResource,
+		newInputs,
+		oldState resource.PropertyMap) error {
+		tok := urn.Type()
+		version := strings.Split(tok.Module().String(), "/")[1]
+		endpoint := resources.CloudAPIEndpoint{
+			Template: fmt.Sprintf(`https://container.googleapis.com/%s/`+
+				`projects/{projectsId}/locations/{locationsId}/clusters/{clustersId}%s`, version, operationSuffix),
+			Values: append(res.Create.Endpoint.Values,
+				resources.CloudAPIResourceParam{
+					Name:    "clustersId",
+					SdkName: "name",
+					Kind:    "path",
+				}),
+		}
+		uri, err := endpoint.URI(newInputs.Mappable(), oldState.Mappable())
+		if err != nil {
+			return err
+		}
+
+		// If key is omitted, use default.
+		_, ok := diffPropPath.Get(resource.NewObjectProperty(newInputs))
+		if !ok {
+			updated, _ := diffPropPath.Add(resource.NewObjectProperty(newInputs), defaultIfMissing(oldState, diffPropPath))
+			newInputs = updated.ObjectValue()
+		}
+
+		properties := map[string]resources.CloudAPIProperty{}
+		err = mergo.Merge(&properties, additionalMetadata)
+		if err != nil {
+			return fmt.Errorf("failed to initialize metadata: %w", err)
+		}
+		properties[apiFieldName] = resources.CloudAPIProperty{SdkName: diffPropPath.String()}
+		body := p.prepareAPIInputs(newInputs, oldState, properties)
+		op, err := retryRequest(p.client, httpMethod, uri, "", body)
+		if err != nil {
+			return fmt.Errorf("error sending request: %s: %q %+v", err, uri, body)
+		}
+
+		_, err = p.waitForResourceOpCompletion(urn, res.Update.CloudAPIOperation, op)
+		if err != nil {
+			return errors.Wrapf(err, "waiting for completion")
+		}
+		return nil
+	}
+}
+
+func updateClusterNestedField(diffPropPath, targetPropPath resource.PropertyPath,
+	defaultIfMissing propValueIfMissingFunc, operationSuffix, httpMethod string) containerCustomUpdateHandlerFunc {
+	return func(p *googleCloudProvider,
+		urn resource.URN,
+		label string,
+		res *resources.CloudAPIResource,
+		newInputs,
+		oldState resource.PropertyMap) error {
+		logging.V(9).Infof("[%s] updateClusterNestedField called for property path: %q with input: %+v", label,
+			diffPropPath.String(), newInputs)
+		tok := urn.Type()
+		version := strings.Split(tok.Module().String(), "/")[1]
+		endpoint := resources.CloudAPIEndpoint{
+			Template: fmt.Sprintf(`https://container.googleapis.com/%s/`+
+				`projects/{projectsId}/locations/{locationsId}/clusters/{clustersId}%s`, version, operationSuffix),
+			Values: append(res.Create.Endpoint.Values,
+				resources.CloudAPIResourceParam{
+					Name:    "clustersId",
+					SdkName: "name",
+					Kind:    "path",
+				}),
+		}
+		uri, err := endpoint.URI(newInputs.Mappable(), oldState.Mappable())
+		if err != nil {
+			return err
+		}
+		// Get the new config object to copy the value for fieldName from
+		newConfigField, ok := diffPropPath.Get(resource.NewObjectProperty(newInputs))
+		if !ok {
+			logging.V(9).Infof("[%s] no update found at path: %q. Assuming the field is deleted.", label,
+				diffPropPath.String())
+			newConfigField = defaultIfMissing(oldState, diffPropPath)
+		}
+		// Overwrite the config object where the one value for fieldName has been updated.
+		newConfig := resource.NewObjectProperty(resource.NewPropertyMapFromMap(nil))
+		if targetPropPath == nil {
+			targetPropPath = diffPropPath
+		}
+		// Now updatedConfigField essentially is a config object with just one field - `fieldName`
+		updatedConfigField, ok := targetPropPath.Add(newConfig, newConfigField)
+		if !ok {
+			return fmt.Errorf("failed to inject config at property path: %q", targetPropPath.String())
+		}
+		logging.V(9).Infof("[%s] staged property value: %+v", label, updatedConfigField.ObjectValue().Mappable())
+		body := p.prepareAPIInputs(updatedConfigField.ObjectValue(), oldState, map[string]resources.CloudAPIProperty{
+			"update": {},
+		})
+		logging.V(9).Infof("[%s] cluster update request body: %v", label, body)
+		op, err := retryRequest(p.client, "PUT", uri, "", body)
+		if err != nil {
+			return fmt.Errorf("error sending request: %s: %q %+v", err, uri, body)
+		}
+
+		_, err = p.waitForResourceOpCompletion(urn, res.Update.CloudAPIOperation, op)
+		if err != nil {
+			return errors.Wrapf(err, "waiting for completion")
+		}
+		return nil
+	}
+}
+
+// The ordering here loosely follows what is in the terraform provider which is itself somewhat arbitrary.
+// There is some underlying value since there are a lot overlapping dependencies on mutations (e.g. setting
+// network policies require the network policy add on etc.). This may need to change over time.
+var clusterUpdateOrder = []string{
+	"masterAuthorizedNetworksConfig",          //
+	"addonsConfig",                            //
+	"autoscaling",                             //
+	"binaryAuthorization",                     //
+	"shieldedNodes",                           //
+	"releaseChannel",                          //
+	"networkConfig.enableIntraNodeVisibility", //
+	"networkConfig.privateIpv6GoogleAccess",   //
+	"networkConfig.enableL4ilbSubsetting",     //
+	"networkConfig.defaultSnatStatus",         //
+	"maintenancePolicy",                       //
+	"locations",                               //
+	"legacyAbac.enabled",                      //
+	"monitoringService",                       //
+	"loggingService",                          //
+	"networkPolicy",                           //
+	//"nodePools",                               // TODO: Don't believe we can allow this to be changed?
+	"initialClusterVersion", //
+	// nodeVersion, // nodePools[*].version
+	"nodeConfig.imageType",   //
+	"notificationConfig",     //
+	"verticalPodAutoscaling", //
+	"meshCertificates",       //
+	"databaseEncryption",     //
+	"workloadIdentifyConfig", //
+	"identityServiceConfig",  //
+	// masterAuth has a pretty imperative API for mutations.
+	// Its mostly deprecated anyway so lets mark it replace-on-change.
+	// "masterAuth",
+	// TODO: Enable these later. See note in overrides.go.
+	//"loggingConfig",             //
+	//"monitoringConfig",          //
+	"resourceLabels",            //
+	"resourceUsageExportConfig", //
+	// Only beta
+	"podSecurityPolicyConfig", //
+	"clusterTelemetry",        //
+	// removeDefaultNodepool,
+	// Additional mutations:
+	"authenticatorGroupsConfig",              //
+	"networkConfig.datapathProvider",         //
+	"networkConfig.dnsConfig",                //
+	"nodeConfig.gcfsConfig",                  //
+	"privateClusterConfig",                   //
+	"networkConfig.serviceExternalIpsConfig", //
+}
+
+func waitForClusterRestingState(
+	p *googleCloudProvider,
+	urn resource.URN,
+	res *resources.CloudAPIResource,
+	inputs,
+	oldState resource.PropertyMap,
+) (map[string]interface{}, error) {
+	ctx, cancelFunc := context.WithTimeout(context.Background(), 15*time.Minute)
+	defer cancelFunc()
+	retryPolicy := backoff.Backoff{
+		Min:    1 * time.Second,
+		Max:    15 * time.Second,
+		Factor: 1.5,
+		Jitter: true,
+	}
+
+	uri, err := res.Read.Endpoint.URI(inputs.Mappable(), oldState.Mappable())
+	if err != nil {
+		return nil, err
+	}
+
+	for {
+		resp, err := readContainerResourceStatus(p, uri)
+		if err != nil {
+			return nil, err
+		}
+
+		status, hasStatus := resp["status"].(string)
+		if hasStatus && isClusterInRestingStatus(status) {
+			return resp, nil
+		} else {
+			_ = p.host.LogStatus(context.Background(), diag.Info, urn,
+				fmt.Sprintf("Waiting for cluster to reach resting state. "+
+					"Current status: %q", status))
+		}
+		select {
+		case <-time.After(retryPolicy.Duration()):
+		case <-ctx.Done():
+			return nil, fmt.Errorf("timeout polling for nodepool resting state: %q", uri)
+		}
+	}
+}
+
+func isClusterInRestingStatus(status string) bool {
+	switch status {
+	case "RUNNING", "DEGRADED", "ERROR":
+		return true
+	}
+	return false
+}
+
+// == Container Nodepool support ==
+
+// nodepoolMutations models the custom mutations supported for Nodepool resource.
 type nodepoolMutations struct {
-	updateHandlers map[string]nodepoolUpdateHandlerFunc
+	updateHandlers map[string]containerCustomUpdateHandlerFunc
 }
 
 func (n nodepoolMutations) mutablePropertyPaths() []string {
@@ -70,7 +400,7 @@ func (n nodepoolMutations) update(
 		logging.V(9).Infof("[%s] detailedDiff: %+v", label, detailedDiff)
 		logging.V(9).Infof("[%s] updateNodepool oldState: %+v", label, oldState)
 
-		for _, key := range updateOrder {
+		for _, key := range nodepoolUpdateOrder {
 			updateHandler, ok := n.updateHandlers[key]
 			if !ok {
 				logging.V(9).Infof("[%s] No handler found for field: %q", label, key)
@@ -125,7 +455,7 @@ func (n nodepoolMutations) update(
 	if readErr != nil {
 		return nil, nil, readErr
 	}
-	resp, err2 := readNodepoolStatus(providerInstance, uri)
+	resp, err2 := readContainerResourceStatus(providerInstance, uri)
 	if err2 != nil {
 		logging.V(9).Infof("[%s] Failed to read nodepool status: %v", label, err2)
 		return nil, nil, fmt.Errorf("failed to retrieve nodepool status: %v. Previous error: %w", err2, err)
@@ -134,7 +464,7 @@ func (n nodepoolMutations) update(
 	return appliedInputs, resp, err
 }
 
-type nodepoolUpdateHandlerFunc func(
+type containerCustomUpdateHandlerFunc func(
 	p *googleCloudProvider,
 	urn resource.URN,
 	label string,
@@ -166,7 +496,7 @@ func defaultValue(defVal resource.PropertyValue) propValueIfMissingFunc {
 
 // Following an ordering here that the terraform provider performs updates in.
 // We have a lot more mutations defined than the terraform providers does though.
-var updateOrder = []string{
+var nodepoolUpdateOrder = []string{
 	"autoscaling",
 	"config.imageType",
 	"config.workloadMetadataConfig",
@@ -191,7 +521,7 @@ func mustParsePropertyPath(pattern string) resource.PropertyPath {
 	return pp
 }
 
-func readNodepoolStatus(
+func readContainerResourceStatus(
 	p *googleCloudProvider,
 	uri string,
 ) (map[string]interface{}, error) {
@@ -225,7 +555,7 @@ func waitForNodepoolRestingState(
 	}
 
 	for {
-		resp, err := readNodepoolStatus(p, uri)
+		resp, err := readContainerResourceStatus(p, uri)
 		if err != nil {
 			return nil, err
 		}
@@ -261,16 +591,19 @@ func updateNodePoolMapping(
 	defaultIfMissing propValueIfMissingFunc,
 	operationSuffix string,
 	httpMethod string,
-) nodepoolUpdateHandlerFunc {
+) containerCustomUpdateHandlerFunc {
 	return func(p *googleCloudProvider,
 		urn resource.URN,
 		label string,
 		res *resources.CloudAPIResource,
 		newInputs,
 		oldState resource.PropertyMap) error {
+		tok := urn.Type()
+		version := strings.Split(tok.Module().String(), "/")[1]
 		endpoint := resources.CloudAPIEndpoint{
-			Template: fmt.Sprintf(`https://container.googleapis.com/v1/`+
-				`projects/{projectsId}/locations/{locationsId}/clusters/{clustersId}/nodePools/{nodePoolsId}%s`, operationSuffix),
+			Template: fmt.Sprintf(`https://container.googleapis.com/%s/`+
+				`projects/{projectsId}/locations/{locationsId}/clusters/{clustersId}/nodePools/{nodePoolsId}%s`,
+				version, operationSuffix),
 			Values: append(res.Create.Endpoint.Values,
 				resources.CloudAPIResourceParam{
 					Name:    "nodePoolsId",
@@ -307,7 +640,7 @@ func updateNodePoolMapping(
 }
 
 func updateNodePoolConfig(diffPropPath, targetPropPath resource.PropertyPath,
-	defaultIfMissing propValueIfMissingFunc) nodepoolUpdateHandlerFunc {
+	defaultIfMissing propValueIfMissingFunc) containerCustomUpdateHandlerFunc {
 	return func(p *googleCloudProvider,
 		urn resource.URN,
 		label string,
@@ -315,8 +648,11 @@ func updateNodePoolConfig(diffPropPath, targetPropPath resource.PropertyPath,
 		newInputs,
 		oldState resource.PropertyMap) error {
 		logging.V(9).Infof("[%s] updateNodePoolConfig called for property path: %q", label, diffPropPath.String())
+		tok := urn.Type()
+		version := strings.Split(tok.Module().String(), "/")[1]
 		endpoint := resources.CloudAPIEndpoint{
-			Template: `https://container.googleapis.com/v1/projects/{projectsId}/locations/{locationsId}/clusters/{clustersId}/nodePools/{nodePoolsId}`,
+			Template: fmt.Sprintf(`https://container.googleapis.com/%s/`+
+				`projects/{projectsId}/locations/{locationsId}/clusters/{clustersId}/nodePools/{nodePoolsId}`, version),
 			Values: append(res.Create.Endpoint.Values,
 				resources.CloudAPIResourceParam{
 					Name:    "nodePoolsId",

provider/pkg/provider/diff.go
@@ -181,6 +181,55 @@ func (d *differ) calculateValueDiff(v *resource.ValueDiff, diffBase, replaceBase
 	return detailedDiff
 }
 
+func diffWithFlattenedKeys(olds, news resource.PropertyMap, detailedDiff map[string]*rpc.PropertyDiff) (map[string]rpc.PropertyDiff_Kind,
+	error) {
+	expandedKeys := map[string]rpc.PropertyDiff_Kind{}
+	for k, diff := range detailedDiff {
+		propPath, err := resource.ParsePropertyPath(k)
+		if err != nil {
+			return nil, err
+		}
+		switch diff.Kind {
+		case rpc.PropertyDiff_ADD, rpc.PropertyDiff_ADD_REPLACE:
+			val, ok := propPath.Get(resource.NewObjectProperty(news))
+			if !ok {
+				return nil, fmt.Errorf("failed to lookup value at path %q on new inputs", k)
+			}
+			addNestedKeys(val, k, diff.Kind, expandedKeys)
+		case rpc.PropertyDiff_DELETE, rpc.PropertyDiff_DELETE_REPLACE:
+			val, ok := propPath.Get(resource.NewObjectProperty(olds))
+			if !ok {
+				return nil, fmt.Errorf("failed to lookup value at path %q on old inputs", k)
+			}
+			addNestedKeys(val, k, diff.Kind, expandedKeys)
+		case rpc.PropertyDiff_UPDATE, rpc.PropertyDiff_UPDATE_REPLACE:
+			expandedKeys[k] = diff.Kind
+		}
+	}
+	return expandedKeys, nil
+}
+
+func addNestedKeys(val resource.PropertyValue, currPath string, kind rpc.PropertyDiff_Kind,
+	expandedKeys map[string]rpc.PropertyDiff_Kind) {
+	if val.IsNull() {
+		return
+	} else if val.IsBool() || val.IsNumber() || val.IsString() || val.IsAsset() || val.IsArchive() {
+		expandedKeys[currPath] = kind
+		return
+	} else if val.IsArray() {
+		for i, e := range val.ArrayValue() {
+			addNestedKeys(e, fmt.Sprintf("%s[%d]", currPath, i), kind, expandedKeys)
+		}
+		return
+	} else if val.IsObject() {
+		propMap := val.ObjectValue()
+		for k, v := range propMap {
+			addNestedKeys(v, fmt.Sprintf("%s.%s", currPath, k), kind, expandedKeys)
+		}
+		return
+	}
+}
+
 // applyDiff produces a new map as a merge of a calculated diff into an existing map of values.
 func applyDiff(values resource.PropertyMap, diff *resource.ObjectDiff) resource.PropertyMap {
 	if diff == nil {

commit aab19f45347eda17e201741c541aa35bda9f6307

Fix bug with aggressive replacement logic introduced in #632

provider/pkg/provider/provider.go
@@ -463,7 +463,8 @@ func (p *googleCloudProvider) Diff(_ context.Context, req *rpc.DiffRequest) (*rp
 	detailedDiff := calculateDetailedDiff(&res, p.resourceMap.Types, diff)
 
 	var mutablePropertyPaths []string
-	if customMutation, hasCustomMutation := customMutations[resourceKey]; hasCustomMutation {
+	customMutation, hasCustomMutation := customMutations[resourceKey]
+	if hasCustomMutation {
 		mutablePropertyPaths = customMutation.mutablePropertyPaths()
 	}
 	logging.V(9).Infof("[%s] DetailedDiff: %+v", label, detailedDiff)
@@ -488,19 +489,22 @@ func (p *googleCloudProvider) Diff(_ context.Context, req *rpc.DiffRequest) (*rp
 		if err != nil {
 			return nil, fmt.Errorf("failed to parse property path: %q: %w", k, err)
 		}
-		found := false
-		for _, p := range mutablePropertyPaths {
-			propPath, err := resource.ParsePropertyPath(p)
-			if err != nil {
-				return nil, fmt.Errorf("failed to parse property path: %q: %w", p, err)
+
+		if hasCustomMutation {
+			found := false
+			for _, p := range mutablePropertyPaths {
+				propPath, err := resource.ParsePropertyPath(p)
+				if err != nil {
+					return nil, fmt.Errorf("failed to parse property path: %q: %w", p, err)
+				}
+				if propPath.Contains(changedPropPath) {
+					found = true
+					break
+				}
 			}
-			if propPath.Contains(changedPropPath) {
-				found = true
-				break
+			if !found {
+				replaces.Add(k)
 			}
-		}
-		if !found {
-			replaces.Add(k)
 		}
 	}
 